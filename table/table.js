6b1b4f97372229989023dcb6103dc1b1071daf9f
Daffodils&Chocolates <101703530+Daffodils-and-Chocolates@users.noreply.github.com>
2024-03-11 14:21:01 +0530

    showed to sir ver

---
+++ table/table.js
@@ -0,0 +1,14 @@
+new DataTable('#example');
+
+table.on('click', 'tbody tr', function () {
+    let data = table.row(this).data();
+ 
+    alert('You clicked on ' + data[0] + "'s row");
+});
+
+function eventFired(type) {
+    let n = document.querySelector('#demo_info');
+    n.innerHTML +=
+        '<div>' + type + ' event - ' + new Date().getTime() + '</div>';
+    n.scrollTop = n.scrollHeight;
+}
